bedc3ca78ab3a74e90cfdb63bd3c2b6fc9948d49
devgreek <adebayokehinde18@gmail.com>
2025-06-06 10:41:46 +0100

    feat: add base64 dependency and implement tests for vanity address generation

--- tests/vanity_tests.rs
+++ tests/vanity_tests.rs
@@ -1,53 +1,11 @@
-use clap::Parser;
-use solana_vanity::{ find_vanity_address, find_vanity_address_with_suffix };
-use std::{ sync::atomic::{ AtomicBool, Ordering }, time::Instant };
-use rayon::prelude::*;
-use solana_sdk::signer::Signer;
+use solana_vanity::{find_vanity_address, find_vanity_address_with_suffix};
 use std::time::Duration;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// The desired prefix for the vanity address (case-sensitive)
-    #[arg(short, long)]
-    // prefix: String,
-    suffix: String,
-
-    /// Number of threads to use (defaults to available CPU cores)
-    #[arg(short, long, default_value_t = num_cpus::get())]
-    threads: usize,
-}
-
-fn main() {
-    let args = Args::parse();
-    // let prefix = args.prefix;
-    let suffix = args.suffix;
-    let num_threads = args.threads;
-
-    println!("Searching for Solana vanity address starting with: \"{}\"", suffix);
-    println!("Using {} threads...", num_threads);
-
-    let result = find_vanity_address_with_suffix(&suffix, num_threads);
-    let pubkey_str = result.keypair.pubkey().to_string();
-    let p_k = result.keypair.secret().as_ref();
-    let secret_key_base64: String = base64::encode(p_k);
-    println!("pk {:?}", secret_key_base64);
-
-    println!("\n🎉 Found a vanity address!");
-    println!("Address: {}", pubkey_str);
-    println!(
-        "Private Key (Base58): {}",
-        bs58::encode(result.keypair.secret().as_ref()).into_string()
-    );
-    println!("Private Key (bytes): {:?}", result.keypair.secret().as_ref());
-    println!("Time elapsed: {:?}", result.elapsed);
-}
 
 #[test]
 fn test_find_vanity_address() {
     // Test finding an address with prefix "A"
     // This should be relatively quick to find
-    let prefix = "Dev";
+    let prefix = "A";
     let num_threads = 4;
     
     let result = find_vanity_address(prefix, num_threads);
@@ -66,7 +24,7 @@ fn test_find_vanity_address() {
 fn test_find_vanity_address_with_suffix() {
     // Test finding an address with suffix "a"
     // This should be relatively quick to find
-    let suffix = "pump";
+    let suffix = "a";
     let num_threads = 4;
     
     let result = find_vanity_address_with_suffix(suffix, num_threads);
